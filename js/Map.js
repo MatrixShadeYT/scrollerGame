b9d11131bfb7aa3dead52d1eef0ae628d733cf92
MatrixShadeYT <mpro9234@gmail.com>
2026-01-11 22:32:11 -0700

    v0.02

---
+++ js/Map.js
@@ -0,0 +1,34 @@
+export class Map {
+    constructor({height,width,tileCount}) {
+        this.tileCount = tileCount || 1;
+        this.height = height;
+        this.width = width;
+        this.mainGrid = this.setupGrid('000');
+        this.offsetGrid = this.setupGrid(['000','000','000','000'],1);
+    }
+    updatePosition(key,position) {
+        if (Number(key) >= 0 && Number(key) < this.tileCount) {
+            this.mainGrid[position.y][position.x] = key;
+            for (let y = 0; y < 2; y++) {
+                for (let x = 0; x < 2; x++) {
+                    for (let i = 0; i < 4; i++) {
+                        this.offsetGrid[position.y+y][position.x+x][3-i] = key
+                    }
+                }
+            }
+        } else {
+            console.error(`Wrong key: ${key}`)
+        }
+    }
+    setupGrid(base,add=0) {
+        const grid = [];
+        for (let y = 0; y < this.height+add; y++) {
+            const list = [];
+            for (let x = 0; x < this.width+add; x++) {
+                list.push(base);
+            }
+            grid.push(list);
+        }
+        return grid;
+    }
+}
--- main.js
+++ main.js
@@ -1,22 +1,43 @@
-import { Game } from './js/Game.js';
+import { Map } from './js/Map.js';
 const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 var canvasRatio = canvas.width/canvas.height;
 canvas.width = canvas.height*(16/10);
-const ctx = canvas.getContext('2d');
-const game = new Game({
-    tileSize: canvasRatio*7.5,
+var tileSize = canvasRatio*7.5;
+const map = new Map({
+    tileCount: 2,
     height: 10,
     width: 16
 });
+for (let i = 0; i < map.height; i++) {
+    map.updatePosition('001',{x:0,y:i});
+}
+for (let i = 0; i < map.height; i++) {
+    map.updatePosition('001',{x:map.width-1,y:i});
+}
+for (let i = 0; i < map.width; i++) {
+    map.updatePosition('001',{x:i,y:map.height-1});
+}
+
+function renderMap() {
+    ctx.fillStyle = '#440000';
+    for (let y = 0; y < map.mainGrid.length; y++) {
+        for (let x = 0; x < map.mainGrid[y].length; x++) {
+            if (map.mainGrid[y][x] == '001') {
+                ctx.fillRect(x*tileSize, y*tileSize, tileSize, tileSize);
+            }
+        }
+    }
+
+}
 
 let lastTime = 0;
 function animate(timeStamp) {
-    window.requestAnimationFrame(animate);
-    ctx.fillStyle = '#060025';
+    //window.requestAnimationFrame(animate);
+    ctx.fillStyle = '#111111';
     ctx.fillRect(0,0,canvas.width,canvas.height);
     const deltaTime = timeStamp-lastTime;
     lastTime = timeStamp;
-    game.update(deltaTime);
-    game.render(ctx);
+    renderMap();
 }
-window.requestAnimationFrame(animate);
+animate(0);
